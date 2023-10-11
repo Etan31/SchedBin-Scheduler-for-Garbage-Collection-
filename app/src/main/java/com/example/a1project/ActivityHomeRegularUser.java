65983976f81994f5f3b2aa8f3c415cf58614b440
Etan31 <tumbagatristanehron31@gmail.com>
2023-10-11 23:19:48 +0800

    Adding switch case to the Admin_Home_activity (not done yet)

--- app/src/main/java/com/example/a1project/ActivityHomeRegularUser.java
+++ app/src/main/java/com/example/a1project/ActivityHomeRegularUser.java
@@ -2,10 +2,13 @@ package com.example.a1project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class ActivityHomeRegularUser extends AppCompatActivity {
 
+  ActivityHomeRegularUser binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/a1project/Admin_Home_activity.java
+++ app/src/main/java/com/example/a1project/Admin_Home_activity.java
@@ -1,14 +1,49 @@
 package com.example.a1project;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.a1project.R.id.nav_calendar;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.example.a1project.databinding.ActivityAdminHomeBinding;
 
 public class Admin_Home_activity extends AppCompatActivity {
 
+    ActivityAdminHomeBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        binding = ActivityAdminHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+//            switch (item.getItemId()) {
+//
+//                case nav_calendar:
+//                    replaceFragment(new ScheduleFragment());
+//                    break;
+//                case R.id.nav_settings:
+//                    replaceFragment(new SettingsFragment());
+//                    break;
+//            }
+
+            return true;
+        });
     }
+
+
+    private void replaceFragment(androidx.fragment.app.Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
