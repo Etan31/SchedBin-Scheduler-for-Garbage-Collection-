c680b4212c600614b655876561103d9c20798e51
Etan31 <tumbagatristanehron31@gmail.com>
2023-10-21 23:08:23 +0800

    added spinner(dropdown menu)

--- app/src/main/java/com/example/a1project/addSchedule_activity.java
+++ app/src/main/java/com/example/a1project/addSchedule_activity.java
@@ -8,16 +8,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class addSchedule_activity extends AppCompatActivity {
+public class addSchedule_activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Button backBtn2;
 
-//    String[] item = {"Biodegradable", "Non-Biodegradable", "Recyclable", "Hazardous"};
-//
-//    AutoCompleteTextView autoCompleteTextView;
-//    ArrayAdapter<String> adapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +24,28 @@ public class addSchedule_activity extends AppCompatActivity {
         backBtn2 = findViewById(R.id.backBtn2);
         backBtn2.setOnClickListener(v -> finish());
 
+        Spinner spinner = findViewById(R.id.spinner_typeofgarbage);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.garbageTypes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
-//        autoCompleteTextView = findViewById(R.id.addSched_garbageType);
-//        adapterItems = new ArrayAdapter<>(this, R.layout.list_item_type_of_garbage, item);
-//
-//        autoCompleteTextView.setAdapter(adapterItems);
-//
-//        autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> {
-//            String item = adapterView.getItemAtPosition(i).toString();
-//            Toast.makeText(addSchedule_activity.this, "Item:" + item, Toast.LENGTH_SHORT).show();
-//        });
 
+        Spinner spinnerDoesNotRepeat = findViewById(R.id.spinner_doesNotRepeat);
+        ArrayAdapter<CharSequence> doesNotRepeatAdapter = ArrayAdapter.createFromResource(this, R.array.doesNotRepeat_array, android.R.layout.simple_spinner_item);
+        doesNotRepeatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDoesNotRepeat.setAdapter(doesNotRepeatAdapter);
+        spinnerDoesNotRepeat.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+//        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
-}   
+}
