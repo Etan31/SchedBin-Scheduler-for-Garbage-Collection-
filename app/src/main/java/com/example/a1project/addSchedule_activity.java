29354ed8eacae9539c7d7376381be2f3be17351e
Etan31 <tumbagatristanehron31@gmail.com>
2023-10-21 23:09:06 +0800

    added spinner(dropdown menu)

--- app/src/main/java/com/example/a1project/addSchedule_activity.java
+++ app/src/main/java/com/example/a1project/addSchedule_activity.java
@@ -40,7 +40,7 @@ public class addSchedule_activity extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
+//        String text = parent.getItemAtPosition(position).toString();
 //        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
     }
 
