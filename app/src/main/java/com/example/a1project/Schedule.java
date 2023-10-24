7f1ec05b852dbf673a89b55c206dbab2a63b466b
Etan31 <tumbagatristanehron31@gmail.com>
2023-10-24 14:03:20 +0800

    added functionality of scheduling

---
+++ app/src/main/java/com/example/a1project/Schedule.java
@@ -0,0 +1,61 @@
+package com.example.a1project;
+
+public class Schedule {
+    private String date;
+    private String time;
+    private String address;
+    private String garbageType;
+    private String repeatType;
+
+    public Schedule() {
+        // Default constructor required for Firebase
+    }
+
+    public Schedule(String date, String time, String address, String garbageType, String repeatType) {
+        this.date = date;
+        this.time = time;
+        this.address = address;
+        this.garbageType = garbageType;
+        this.repeatType = repeatType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGarbageType() {
+        return garbageType;
+    }
+
+    public void setGarbageType(String garbageType) {
+        this.garbageType = garbageType;
+    }
+
+    public String getRepeatType() {
+        return repeatType;
+    }
+
+    public void setRepeatType(String repeatType) {
+        this.repeatType = repeatType;
+    }
+}
--- app/src/main/java/com/example/a1project/addSchedule_activity.java
+++ app/src/main/java/com/example/a1project/addSchedule_activity.java
@@ -2,18 +2,38 @@ package com.example.a1project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import java.util.Calendar;
+import java.util.Locale;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
 
 public class addSchedule_activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Button backBtn2;
+    private Calendar calendar;
 
 
     @Override
@@ -21,14 +41,32 @@ public class addSchedule_activity extends AppCompatActivity implements AdapterVi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
 
+        //for storing schedules to firebase
+        FirebaseApp.initializeApp(this);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("schedules");
+
+
+        TextInputLayout dateInputLayout = findViewById(R.id.layout_addSched_date);
+        AutoCompleteTextView dateAutoCompleteTextView = findViewById(R.id.addSched_date);
+        TextInputLayout timeTextInputLayout = findViewById(R.id.layout_addSched_time);
+        AutoCompleteTextView timeAutoCompleteTextView = findViewById(R.id.addSched_time);
+        TextInputLayout addressTextInputLayout = findViewById(R.id.layout_addSched_address);
+        AutoCompleteTextView addressAutoCompleteTextView = findViewById(R.id.addSched_address);
+        Button addScheduleButton = findViewById(R.id.add_schedule_Btn);
+
+
+        //end of line for storing schedules to firebase
+
+
         backBtn2 = findViewById(R.id.backBtn2);
         backBtn2.setOnClickListener(v -> finish());
 
-        Spinner spinner = findViewById(R.id.spinner_typeofgarbage);
+        //for displaying spinners
+        Spinner spinner_type_of_garbage = findViewById(R.id.spinner_typeofgarbage);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.garbageTypes, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        spinner_type_of_garbage.setAdapter(adapter);
+        spinner_type_of_garbage.setOnItemSelectedListener(this);
 
 
         Spinner spinnerDoesNotRepeat = findViewById(R.id.spinner_doesNotRepeat);
@@ -36,8 +74,77 @@ public class addSchedule_activity extends AppCompatActivity implements AdapterVi
         doesNotRepeatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDoesNotRepeat.setAdapter(doesNotRepeatAdapter);
         spinnerDoesNotRepeat.setOnItemSelectedListener(this);
+        //end of //for displaying spinners
+
+        //for adding data to firebase
+        addScheduleButton.setOnClickListener(view -> {
+            // Get values from input fields
+            String date = dateAutoCompleteTextView.getText().toString();
+            String time = timeAutoCompleteTextView.getText().toString();
+            String address = addressAutoCompleteTextView.getText().toString();
+            String garbageType = spinner_type_of_garbage.getSelectedItem().toString();
+            String repeatType = spinnerDoesNotRepeat.getSelectedItem().toString();
+
+            // Create a unique key for the schedule
+            String scheduleKey = databaseReference.push().getKey();
+
+            // Create a Schedule object
+            Schedule schedule = new Schedule(date, time, address, garbageType, repeatType);
+
+            // Save the schedule to Firebase
+            databaseReference.child(scheduleKey).setValue(schedule);
+
+             Toast.makeText(this, "Schedule added to Firebase", Toast.LENGTH_SHORT).show();
+        });
+
+
+
+        //for clicking the date there will be a calendar to choose date
+        calendar = Calendar.getInstance();
+        dateAutoCompleteTextView.setOnClickListener(v -> {
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePicker = new DatePickerDialog(addSchedule_activity.this, (view, year1, month1, day1) -> {
+                // Display the selected date in the format mm/dd/yyyy
+                String selectedDate = String.format(Locale.US, "%02d/%02d/%d", month1 + 1, day1, year1);
+                dateAutoCompleteTextView.setText(selectedDate);
+            }, year, month, day);
+
+            datePicker.show();
+            Log.d("DatePicker", "Date picker dialog opened.");
+        });
+
+        timeAutoCompleteTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour = calendar.get(Calendar.HOUR);
+                int minute = calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePicker = new TimePickerDialog(addSchedule_activity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar selectedTime = Calendar.getInstance();
+                        selectedTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        selectedTime.set(Calendar.MINUTE, minute);
+
+                        // Display the selected time in the 12-hour format with AM/PM
+                        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a", Locale.US);
+                        String selectedTimeStr = timeFormat.format(selectedTime.getTime());
+                        timeAutoCompleteTextView.setText(selectedTimeStr);
+                    }
+                }, hour, minute, false);
+
+                timePicker.show();
+            }
+        });
     }
 
+
+
+
+    //for displaying spinners
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 //        String text = parent.getItemAtPosition(position).toString();
