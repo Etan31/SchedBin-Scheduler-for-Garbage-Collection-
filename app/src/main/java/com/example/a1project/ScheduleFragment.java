519d582b2eadca49ed1a0588d3f22cb5f309268f
Etan31 <tumbagatristanehron31@gmail.com>
2023-11-09 22:11:50 +0800

    deleted TODO's

--- app/src/main/java/com/example/a1project/ScheduleFragment.java
+++ app/src/main/java/com/example/a1project/ScheduleFragment.java
@@ -37,12 +37,12 @@ public class ScheduleFragment extends Fragment {
     private TableLayout tableLayout;
 
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -70,7 +70,7 @@ public class ScheduleFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ScheduleFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static ScheduleFragment newInstance(String param1, String param2) {
         ScheduleFragment fragment = new ScheduleFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/example/a1project/SettingsFragment.java
+++ app/src/main/java/com/example/a1project/SettingsFragment.java
@@ -21,12 +21,12 @@ public class SettingsFragment extends Fragment {
 
     private Button logout;
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -42,7 +42,7 @@ public class SettingsFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment SettingsFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
