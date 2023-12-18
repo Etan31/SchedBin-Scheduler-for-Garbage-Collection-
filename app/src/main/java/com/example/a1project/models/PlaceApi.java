e03d14f93ae1a2dc3634097c5a000a36a6161186
Etan31 <tumbagatristanehron31@gmail.com>
2023-12-18 11:35:45 +0800

    deleted unnecessary comments and revert hiding the API_KEY.

--- app/src/main/java/com/example/a1project/models/PlaceApi.java
+++ app/src/main/java/com/example/a1project/models/PlaceApi.java
@@ -1,70 +1,4 @@
-package com.example.a1project.models;//package com.example.a1project.models;
-//
-//import android.util.Log;
-//
-//import org.json.JSONArray;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.MalformedURLException;
-//import java.net.URL;
-//import java.util.ArrayList;
-//
-//public class PlaceApi {
-//
-//    public ArrayList<String> autoComplete(String input){
-//        ArrayList<String> arrayList=new ArrayList();
-//        HttpURLConnection connection=null;
-//        StringBuilder jsonResult=new StringBuilder();
-//        try {
-//            StringBuilder sb=new StringBuilder("https://maps.googleapis.com/maps/api/place/autocomplete/json?");
-//            sb.append("input="+input);
-//            sb.append("&key=AIzaSyAGFBw4hTu40ml3yin8cOc-XKsmKmd_8t8");
-//            URL url=new URL(sb.toString());
-//            connection=(HttpURLConnection)url.openConnection();
-//            InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream());
-//
-//            int read;
-//
-//            char[] buff=new char[1024];
-//            while ((read=inputStreamReader.read(buff))!=-1){
-//                jsonResult.append(buff,0,read);
-//            }
-//
-//            Log.d("JSon",jsonResult.toString());
-//        }
-//        catch (MalformedURLException e){
-//            e.printStackTrace();
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        finally {
-//            if(connection!=null){
-//                connection.disconnect();
-//            }
-//        }
-//
-//        try {
-//            JSONObject jsonObject=new JSONObject(jsonResult.toString());
-//            JSONArray prediction=jsonObject.getJSONArray("predictions");
-//            for(int i=0;i<prediction.length();i++){
-//                arrayList.add(prediction.getJSONObject(i).getString("description"));
-//            }
-//        }
-//        catch (JSONException e){
-//            e.printStackTrace();
-//        }
-//
-//        return arrayList;
-//    }
-//
-//
-//}
-
+package com.example.a1project.models;
 
 import android.util.Log;
 
@@ -81,7 +15,7 @@ import java.util.ArrayList;
 
 public class PlaceApi {
 
-    private static final String API_KEY = "YOUR_API_KEY";
+    private static final String API_KEY = "AIzaSyAGFBw4hTu40ml3yin8cOc-XKsmKmd_8t8";
 
     public ArrayList<String> autoComplete(String input){
         ArrayList<String> arrayList=new ArrayList();
