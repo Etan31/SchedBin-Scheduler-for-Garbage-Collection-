24cc212b66299f1703fb060d5557d812d5cf039d
Etan31 <tumbagatristanehron31@gmail.com>
2024-02-07 08:54:19 +0800

    change package name

--- app/src/main/java/com/schedBin/a1project/Admin_Home_activity.java
+++ app/src/main/java/com/schedBin/a1project/Admin_Home_activity.java
@@ -1,20 +1,15 @@
-package com.example.a1project;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.motion.widget.MotionLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+package com.schedBin.a1project;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-//import com.example.a1project.databinding.ActivityAdminHomeBinding;
-import com.example.a1project.databinding.ActivityAdminHomeBinding;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import androidx.constraintlayout.motion.widget.MotionLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.schedBin.a1project.databinding.ActivityAdminHomeBinding;
 
 public class Admin_Home_activity extends AppCompatActivity implements ScheduleFragment.OnFabClickListener  {
 
